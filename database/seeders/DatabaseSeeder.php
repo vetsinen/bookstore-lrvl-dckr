3df1340b703c040a0434f93a2c18f2f1b0b9e0a0
vetsinen <vetsinen@gmail.com>
2023-04-12 16:14:54 +0300

    initial2

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the applications's database
+     * 
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+
+}
